f308916432a6189a9dfd90c086ea1fc3eba04781
dilution0216 <hglad0216@naver.com>
2023-10-13 21:34:43 +0900

    feat : Controller 구현

---
+++ src/main/java/com/sparta/wantedpreonboardingbackend/controller/JobController.java
@@ -0,0 +1,57 @@
+package com.sparta.wantedpreonboardingbackend.controller;
+
+import com.sparta.wantedpreonboardingbackend.entity.Application;
+import com.sparta.wantedpreonboardingbackend.entity.Job;
+import com.sparta.wantedpreonboardingbackend.entity.User;
+import com.sparta.wantedpreonboardingbackend.service.JobService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+    private JobService jobService;
+
+    @GetMapping
+    public List<Job> getJobs(@RequestParam(required = false) String search) {
+        return jobService.getJobs(search);
+    }
+
+    @GetMapping("/{id}")
+    public Job getJobById(@PathVariable Long id) {
+        return jobService.getJobById(id)
+                .orElseThrow(() -> new RuntimeException("Job not found"));
+    }
+
+    @PostMapping
+    public Job createJob(@RequestBody Job job) {
+        return jobService.saveJob(job);
+    }
+
+    @PutMapping("/{id}")
+    public Job updateJob(@PathVariable Long id, @RequestBody Job updatedJob) {
+        return jobService.getJobById(id).map(job -> {
+            job.setPosition(updatedJob.getPosition());
+            job.setReward(updatedJob.getReward());
+            job.setDescription(updatedJob.getDescription());
+            job.setTechnology(updatedJob.getTechnology());
+            return jobService.saveJob(job);
+        }).orElseThrow(() -> new RuntimeException("Job not found"));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteJob(@PathVariable Long id) {
+        jobService.deleteJob(id);
+    }
+
+    @PostMapping("/{id}/apply")
+    public Application applyToJob(@PathVariable Long id, @RequestBody User user) {
+        Job job = jobService.getJobById(id)
+                .orElseThrow(() -> new RuntimeException("Job not found"));
+        return jobService.applyToJob(user, job);
+    }
+}
