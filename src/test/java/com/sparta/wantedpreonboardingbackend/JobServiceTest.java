e95f6fc2c705fbb3e259a92df73224c7da7d7ee8
dilution0216 <hglad0216@naver.com>
2023-10-13 22:07:32 +0900

    feat : ServiceTest CRUD 검증 추가 구현

--- src/test/java/com/sparta/wantedpreonboardingbackend/JobServiceTest.java
+++ src/test/java/com/sparta/wantedpreonboardingbackend/JobServiceTest.java
@@ -12,9 +12,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class JobServiceTest {
@@ -39,5 +42,32 @@ public class JobServiceTest {
         List<Job> jobs = jobService.getJobs(null);
         assertEquals(1, jobs.size());
         assertEquals("Software Engineer", jobs.get(0).getPosition());
+    }
+
+    @Test
+    public void getJobByIdTest() {
+        Job job = new Job();
+        job.setPosition("Software Engineer");
+        when(jobRepository.findById(1L)).thenReturn(Optional.of(job));
+
+        Job foundJob = jobService.getJobById(1L).orElse(null);
+        assertEquals("Software Engineer", foundJob.getPosition());
+    }
+
+    @Test
+    public void saveJobTest() {
+        Job job = new Job();
+        job.setPosition("Software Engineer");
+        when(jobRepository.save(any(Job.class))).thenReturn(job);
+
+        Job savedJob = jobService.saveJob(job);
+        assertEquals("Software Engineer", savedJob.getPosition());
+    }
+
+    @Test
+    public void deleteJobTest() {
+        doNothing().when(jobRepository).deleteById(1L);
+        jobService.deleteJob(1L);
+        verify(jobRepository, times(1)).deleteById(1L);
     }
 }
